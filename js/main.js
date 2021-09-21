38be61e5af40c3527eb3688d7c3db4f701ffdb7d
mallikarjuna-sharma <rmalliksharma96@gmail.com>
2021-09-21 14:15:20 +0530

    updated mobile portfolio , EQUIPMENT

--- js/main.js
+++ js/main.js
@@ -5,10 +5,10 @@
   /*------------------
         Preloader
     --------------------*/
-  $(window).on("load", function () {
-    $(".loader").fadeOut();
-    $("#preloder").delay(200).fadeOut("slow");
-  });
+  // $(window).on("load", function () {
+  //   $(".loader").fadeOut();
+  //   $("#preloder").delay(200).fadeOut("slow");
+  // });
 
   /*------------------
         Background Set
